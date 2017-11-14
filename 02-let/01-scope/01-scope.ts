f4d04539948b6b5ed49ab0fbdc9438d24988c237
Daniel Avalos <mavalos_daniel@hotmail.com>
2017-11-13 21:20:40 -0600

    Adding let scope

---
+++ 02-let/01-scope/01-scope.ts
@@ -0,0 +1,19 @@
+{
+    var a = "block";
+}
+console.log(a);
+
+// function hello() {
+//     var a = "function";
+// }
+// hello();
+// console.log(a);
+
+// function hello() {
+//     var a = "function";
+//     for (var i = 0; i < 10; i++) {
+//         var a = "block";
+//     }
+//     console.log(a);
+// }
+// hello();
